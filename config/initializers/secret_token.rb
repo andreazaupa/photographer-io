14599d7285e3a3f7106cc2d3138932ce47364356
Robert May <robotmay@gmail.com>
2013-04-30 00:15:29 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iso::Application.config.secret_key_base = '5485469b6df999253ec232295a4a96e02344985cf5d6a33637dbfa7c6070876decaf027ba19b9a31a9c490474f179e1462171a78cc24feada49e3beb2a20f33e'
